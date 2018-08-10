25fd8a7f74b252e3edb52adeaf6c39f4c26dca42
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-10 08:57:03 -0700

    initial commit -m "created register function to create a new account for the user, check for empty username and password and return the appropriate error message"

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
 const axios = require('axios');
-
+const server = express.Router();
 const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig');
+
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,7 +15,44 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+	
+if(!req.body.username || !req.body.password){
+        res.status(401).json({message: 'Please do not leave any fields empty. Provide a username and password'});
+        }
+
+        else{
+        const credentials = req.body;
+        const hash = bcrypt.hashSync(credentials.password, 14);  //hasing password using bcrypt
+        credentials.password = hash;
+
+
+        db('users')
+        .insert(credentials)
+
+        .then(ids =>{
+
+                db('users')
+                .where('id', ids[0])
+                .first()
+                .then(user => {
+		let userCreated = {};
+	        userCreated={id: user.id, username: user.username};
+                //const token = generateToken(user);
+
+                res.status(200).json(userCreated);
+                })
+        })
+
+        .catch(err =>{
+        if(err.message.includes('UNIQUE constraint failed: users.username')) res.status(500).json({errorMessage:"username already taken, use another username"});  //checking if the username is alread taken
+
+        else res.status(500).json(err);
+        });
+
+}      
 }
+
+
 
 function login(req, res) {
   // implement user login
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const express = require('express');
+//const morgan = require('morgan');
+const cors = require('cors');
 const { server } = require('./server');
+const app = express();
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`\n=== Server listening on port ${port}\n`);
+app.use(express.json());
+//app.use(morgan('dev'));
+app.use(cors());
+app.use('/api', server);
+
+
+//const { server } = require('./server');
+
+//const port = process.env.PORT || 5000;
+
+
+
+
+server.listen(4004, () => {
+  console.log(`API listening on port 4004`);
 });
